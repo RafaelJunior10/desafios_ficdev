12327c2d67e72657182d1c7a9ca6f691623a1390
RafaelJunior10 <131674208+RafaelJunior10@users.noreply.github.com>
2023-06-02 10:57:57 -0400

    Add files via upload

---
+++ Capitulo05/src/capitulo05/Carro.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capitulo05;
+
+/**
+ *
+ * @author ficdev
+ */
+public class Carro {
+
+    public String modelo;
+    protected int ano;
+    private String fabricante;
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getFabricante() {
+        return this.fabricante;
+    }
+    
+}
