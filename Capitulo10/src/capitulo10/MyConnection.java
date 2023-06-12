4b1255096f8d42f9e44bb527970deb7365156a3b
RafaelJunior10 <131674208+RafaelJunior10@users.noreply.github.com>
2023-06-12 10:42:37 -0400

    Atividade10

---
+++ Capitulo10/src/capitulo10/MyConnection.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package capitulo10;
+
+/**
+ *
+ * @author ficdev
+ */
+
+import java.sql.*;
+
+public class MyConnection {
+
+    public static void main(String[] args) {
+        try {
+            //Class.forName("org.postgresql.Driver"); // registra o driver
+  
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ficdatabase", "postgres", "123456"); // abre conexão
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM escola"); // executa comando SQL
+            while (rs.next()) { //manipula resultado
+                int id = rs.getInt("idescola");
+                String nome = rs.getString("nomeescola");
+                System.out.println(id + ", " + nome);
+            }
+            conn.close(); // fecha conexão
+        } catch (Exception e) {
+            System.err.println("Erro: " + e.getMessage());
+        }
+    }
+}
